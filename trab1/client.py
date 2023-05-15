d0443ac12e90916d588ed81e4d6385cc3a163d53
Renan M. Gomes <renangomes.es@gmail.com>
2023-05-15 03:05:42 -0300

    morre praga

---
+++ trab1/client.py
@@ -0,0 +1,183 @@
+from concurrent import futures
+from typing import List, Union, Tuple
+
+import argparse
+import fedlearn_grpc_pb2
+import fedlearn_grpc_pb2_grpc
+import grpc
+import numpy as np
+import random
+import tensorflow as tf
+import uuid
+
+uuid1 = uuid.uuid1()
+client_id = str(uuid1.int >> 96)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--server", type=str, default="127.0.0.1:8080")
+parser.add_argument("--address", type=str, default="127.0.0.1")
+parser.add_argument("--port", type=str, default="50051")
+args = parser.parse_args()
+
+
+class FederatedLearningClient(fedlearn_grpc_pb2_grpc.clientServicer):
+    def __init__(self, ip: str, port: int, client_id: str):
+        self.ip = ip
+        self.port = port
+        self.client_id = client_id
+
+        self.model = self.define_model((28, 28, 1), 10)
+        self.x_train = np.array([])
+        self.y_train = np.array([])
+        self.x_test = np.array([])
+        self.y_test = np.array([])
+        self.epochs = 0
+
+        self.current_round = 0
+        self.weights = np.array([])
+
+        self.dataset: tuple = tf.keras.datasets.mnist.load_data()
+        self.x_train, self.y_train = self.dataset[0]
+        self.x_test, self.y_test = self.dataset[1]
+        self.x_train, self.y_train, self.x_test, self.y_test = self.normalize_dataset(
+            self.x_train, self.y_train, self.x_test, self.y_test
+        )
+        self.start = 0
+        self.end = 0
+
+    def normalize_dataset(
+        self,
+        x_train,
+        y_train,
+        x_test,
+        y_test,
+    ) -> tuple:
+        x_tr = (
+            x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+            / 255.0
+        )
+        x_te = (
+            x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1) / 255.0
+        )
+
+        y_tr = tf.one_hot(y_train.astype(np.int32), depth=10)
+        y_te = tf.one_hot(y_test.astype(np.int32), depth=10)
+
+        return x_tr, y_tr, x_te, y_te
+
+    def TrainingStart(self, request, context):
+        self.start = request.start
+        self.end = request.end
+
+        self.epochs = request.epochs
+
+        self.fit()
+
+        weights_list = self.model.get_weights()
+
+        weights_bytes_list = [np.array(weights).tobytes() for weights in weights_list]
+
+        num_samples = self.x_train[self.start : self.end].shape[0]
+
+        return fedlearn_grpc_pb2.TrainingStartResponse(
+            weights=weights_bytes_list, num_samples=num_samples
+        )
+
+    def ModelEvaluation(self, request, context):
+        aggregated_weights = request.weights
+
+        received_weights_list = []
+        for weights_bytes in aggregated_weights:
+            weights_array = np.frombuffer(weights_bytes, dtype=np.float32)
+            received_weights_list.append(weights_array)
+
+        self.x_test = np.frombuffer(request.test_data, dtype=np.uint32).reshape(
+            (-1, 28, 28)
+        )
+        self.y_test = np.frombuffer(request.test_labels, dtype=np.uint32)
+        accuracy = self.evaluate(received_weights_list)
+        print(f"Client {self.client_id} - Accuracy: {accuracy}")
+        return fedlearn_grpc_pb2.ModelEvaluationResponse(accuracy=accuracy)
+
+    def fit(self):
+        self.model.fit(
+            self.x_train[self.start : self.end],
+            self.y_train[self.start : self.end],
+            epochs=self.epochs,
+        )
+
+    def evaluate(self, aggregated_weights):
+        self.model.set_weights(aggregated_weights)
+        return self.model.evaluate(self.x_test, self.y_test)[1]
+
+    def define_model(self, input_shape, num_classes):
+        model = tf.keras.models.Sequential()
+        model.add(
+            tf.keras.layers.Conv2D(
+                32,
+                kernel_size=(3, 3),
+                activation="relu",
+                kernel_initializer="he_uniform",
+                input_shape=input_shape,
+            )
+        )
+        model.add(tf.keras.layers.MaxPool2D((2, 2)))
+        model.add(tf.keras.layers.Flatten())
+        model.add(
+            tf.keras.layers.Dense(
+                100, activation="relu", kernel_initializer="he_uniform"
+            )
+        )
+        model.add(tf.keras.layers.Dense(num_classes, activation="softmax"))
+        opt = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9)
+        model.compile(
+            optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"]
+        )
+        return model
+
+
+def main() -> None:
+    server = args.server
+    address = args.address
+    port = args.port
+
+    with grpc.insecure_channel(server) as channel:
+        stub = fedlearn_grpc_pb2_grpc.apiStub(channel)
+        print(f"Client {client_id} connected to server {server}")
+
+        client = FederatedLearningClient(address, port, client_id)
+
+        options = [
+            ("grpc.max_receive_message_length", 500 * 1024 * 1024),  # 100 MB
+        ]
+
+        clientGrpc = grpc.server(
+            futures.ThreadPoolExecutor(max_workers=1), options=options
+        )
+        fedlearn_grpc_pb2_grpc.add_clientServicer_to_server(client, clientGrpc)
+        clientGrpc.add_insecure_port(f"{address}:{port}")
+        clientGrpc.start()
+
+        response = stub.ClientRegister(
+            fedlearn_grpc_pb2.ClientRegisterRequest(
+                ip=client.ip, port=client.port, client_id=client.client_id
+            )
+        )
+
+        confirmation_code = response.confirmation_code
+        current_round = response.current_round
+
+        client.current_round = current_round
+
+        if confirmation_code != 0:
+            return
+
+        print(
+            f"Client {client_id} registered with confirmation code {confirmation_code}"
+        )
+
+        print(f"Client {client_id} listening on {address}:{port}")
+        clientGrpc.wait_for_termination()
+
+
+main()
---
+++ trab1/server.py
@@ -0,0 +1,256 @@
+from concurrent import futures
+from typing import List, Union, Tuple
+
+import fedlearn_grpc_pb2
+import fedlearn_grpc_pb2_grpc
+import grpc
+import numpy as np
+import random
+import tensorflow as tf
+import threading
+import time
+
+
+class Client:
+    def __init__(
+        self, ip: str, port: int, client_id: str, server_address: str, epochs: int
+    ):
+        self.ip: str = ip
+        self.port: int = port
+        self.client_id: str = client_id
+        self.start: int = 0
+        self.end: int = 0
+        self.epochs: int = epochs
+        self.server_address: str = server_address
+        self.stub = None
+        self.thread = threading.Thread(target=self.start_stub)
+        self.connect()
+
+    def start_stub(self):
+        channel = grpc.insecure_channel(self.ip + ":" + str(self.port))
+        self.stub = fedlearn_grpc_pb2_grpc.clientStub(channel)
+
+    def trainingStart(self):
+        request = self.stub.TrainingStart(
+            fedlearn_grpc_pb2.TrainingStartRequest(
+                start=self.start,
+                end=self.end,
+                epochs=self.epochs,
+            )
+        )
+        response = self.stub.TrainingStart(request)
+        weights_bytes_list = response.weights
+
+        received_weights_list = []
+        for weights_bytes in weights_bytes_list:
+            weights_array = np.frombuffer(weights_bytes, dtype=np.float32)
+            received_weights_list.append(weights_array)
+
+        return received_weights_list, response.num_samples
+
+    def modelEvaluation(self, aggregated_weights):
+        try:
+            aggregated_weights_bytes = [
+                weight.tobytes() for weight in aggregated_weights
+            ]
+
+            request = self.stub.ModelEvaluation(
+                fedlearn_grpc_pb2.ModelEvaluationRequest(
+                    aggregated_weights=aggregated_weights_bytes
+                )
+            )
+            response = self.stub.ModelEvaluation(request)
+            return response.accuracy
+        except Exception as e:
+            print(e)
+
+    def connect(self):
+        self.thread.start()
+
+
+class FederatedLearningServer(fedlearn_grpc_pb2_grpc.apiServicer):
+    def __init__(
+        self,
+        epochs: int,
+        min_clients: int,
+        max_clients: int,
+        per_round_clients: int,
+        max_rounds: int,
+        accuracy_threshold: float,
+    ) -> None:
+        if max_clients < min_clients:
+            raise ValueError(
+                "max_clients must be greater than or equal to min_clients."
+            )
+
+        self.epochs: int = epochs
+        self.min_clients: int = min_clients
+        self.max_clients: int = max_clients
+        self.per_round_clients: int = per_round_clients
+        self.max_rounds: int = max_rounds
+        self.accuracy_threshold: float = accuracy_threshold
+
+        self.clients: List[Client] = []
+        self.dataset: tuple = tf.keras.datasets.mnist.load_data()
+        self.current_round: int = 0
+        self.rpc = fedlearn_grpc_pb2
+        self.last_aggregated_weights = np.array([])
+
+        self.main_thread = threading.Thread(target=self.run)
+        self.main_thread.start()
+
+    def run(self) -> None:
+        while True:
+            if self.has_sufficient_clients():
+                round_clients = self.choose_clients()
+                self.split_dataset(self.clients)
+
+                threads = []
+                results = self.clients_work_now(round_clients)
+                x, y = zip(*results)
+
+                print(y)
+
+                self.last_aggregated_weights = self.federated_average(x, y)
+
+                results = self.clients_evaluate_now(
+                    self.clients, self.last_aggregated_weights
+                )
+
+                average_accuracy = np.mean(results)
+
+                if average_accuracy >= self.accuracy_threshold:
+                    break
+
+                self.current_round += 1
+
+                if self.current_round >= self.max_rounds:
+                    break
+
+            time.sleep(0.1)
+
+    def clients_evaluate_now(self, clients, weights) -> List[int]:
+        threads = []
+        results = []
+
+        for client in clients:
+            thread = threading.Thread(
+                target=self.execute_model_evaluation, args=(client, weights, results)
+            )
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        return [n for _, n in results]
+
+    def clients_work_now(self, clients):
+        threads = []
+        results = []
+
+        for client in clients:
+            thread = threading.Thread(
+                target=self.execute_training_start, args=(client, results)
+            )
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        print("All clients finished training.")
+        print(results)
+        return results
+
+    def execute_training_start(self, client: Client, results: List):
+        weights, num_samples = client.trainingStart()
+        print(f"Client {client.client_id} finished training.")
+        results.append((weights, num_samples))
+
+    def execute_model_evaluation(
+        self, client: Client, weights: np.ndarray, results: List
+    ):
+        accuracy = client.modelEvaluation(weights)
+        results.append(accuracy)
+
+    def federated_average(self, weights, num_samples):
+        total_samples = sum(num_samples)
+        num_arrays = len(weights[0])
+        averaged_weights = []
+
+        for i in range(num_arrays):
+            weighted_sum = np.zeros_like(weights[0][i])
+            for j, w in enumerate(weights):
+                weighted_sum += w[i] * num_samples[j]
+            averaged_weights.append(weighted_sum / total_samples)
+
+        return averaged_weights, total_samples
+
+    def has_sufficient_clients(self) -> bool:
+        return len(self.clients) >= self.min_clients
+
+    def ClientRegister(self, request, context):
+        ip: str = request.ip
+        port: int = request.port
+        client_id: str = request.client_id
+
+        confirmation_code: int = self.add_client(ip, port, client_id)
+        current_round: int = self.current_round
+
+        return self.rpc.ClientRegisterResponse(
+            confirmation_code=confirmation_code, current_round=current_round
+        )
+
+    def choose_clients(self) -> List[Client]:
+        if len(self.clients) < self.per_round_clients:
+            raise ValueError("There are not enough clients to choose from.")
+
+        chosen_clients = random.sample(self.clients, self.per_round_clients)
+        return chosen_clients
+
+    def add_client(self, ip: str, port: int, client_id: str) -> int:
+        if len(self.clients) >= self.max_clients:
+            return -1
+
+        client = Client(ip, port, client_id, "localhost:8080", self.epochs)
+        self.clients.append(client)
+        print(f"Added client {client_id}.")
+        return 0
+
+    def split_dataset(self, clients) -> None:
+        (x_train, y_train), (x_test, y_test) = self.dataset
+        num_clients: int = len(clients)
+
+        chunk_size = len(x_train) // num_clients
+
+        for i, client in enumerate(clients):
+            start = i * chunk_size
+            end = (i + 1) * chunk_size
+
+            client.start = start
+            client.end = end
+
+
+def serve():
+    options = [
+        ("grpc.max_receive_message_length", 500 * 1024 * 1024),  # 100 MB
+    ]
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=4), options=options)
+    fedlearn_grpc_pb2_grpc.add_apiServicer_to_server(
+        FederatedLearningServer(
+            epochs=1,
+            min_clients=2,
+            max_clients=4,
+            per_round_clients=2,
+            max_rounds=5,
+            accuracy_threshold=90,
+        ),
+        server,
+    )
+    server.add_insecure_port("[::]:8080")
+    server.start()
+    server.wait_for_termination()
+
+
+serve()
