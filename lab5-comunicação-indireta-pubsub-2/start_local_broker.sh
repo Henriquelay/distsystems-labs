536bf758f06441e23c4fdb4e5e5d8eb521596777
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2023-06-03 23:18:46 -0300

    boas praticadas

--- lab5-comunicação-indireta-pubsub-2/start_local_broker.sh
+++ lab5-comunicação-indireta-pubsub-2/start_local_broker.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-# Verifica se o Mosquitto está instalado
-if ! command -v mosquitto &> /dev/null; then
-    echo "O Mosquitto não está instalado. Por favor, instale-o antes de executar este script."
-    exit 1
-fi
-
-# Inicia o serviço do Mosquitto
-sudo service mosquitto start
-
-# Verifica se o serviço foi iniciado corretamente
-if [ $? -eq 0 ]; then
-    echo "Broker MQTT local iniciado com sucesso."
-else
-    echo "Falha ao iniciar o broker MQTT local."
-fi
+docker compose up -d
--- lab5-comunicação-indireta-pubsub-2/stop_local_broker.sh
+++ lab5-comunicação-indireta-pubsub-2/stop_local_broker.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-# Para o serviço do Mosquitto
-sudo service mosquitto stop
-
-# Verifica se o serviço foi interrompido corretamente
-if [ $? -eq 0 ]; then
-    echo "Broker MQTT local foi interrompido com sucesso."
-else
-    echo "Falha ao interromper o broker MQTT local."
-fi
+docker compose down
